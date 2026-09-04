59a3576e92282a07f86d6aa6a91b4fb2e0ca3cb2
Vale Posada <valeposadacadavid7@gmail.com>
2026-09-04 13:17:18 -0500

    diagrama.py

---
+++ diagrama.py
@@ -0,0 +1,27 @@
+Flujo del programa
+BUSCAR POR AUTOR
+      ↓
+Usuario escribe autor
+      ↓
+Se recorren los libros
+      ↓
+¿Coincide el autor?
+      ↓
+Sí → Mostrar ID y libro
+
+
+DEVOLVER LIBRO
+      ↓
+Usuario escribe ID préstamo
+      ↓
+Se busca el préstamo
+      ↓
+Se pregunta cantidad de días
+      ↓
+Se cambia a "DEVUELTO"
+      ↓
+¿Más de 7 días?
+   ↙️          ↘️
+ NO            SÍ
+ ↓              ↓
+$0       (días - 7) × $1000
